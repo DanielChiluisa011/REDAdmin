c272e324a79072a129de918b245cdbef0de1255f
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-04-06 09:27:36 -0500

    Commit CambioPuertos Paginas

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -1,4 +1,4 @@
-var socket = io.connect("http://localhost:8080",{"forceNew": true});
+var socket = io.connect("http://localhost:80",{"forceNew": true});
 var lstWaste=[];
 var lstOrders=[];
 var lstObjOrders=[];

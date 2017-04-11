f70bf0ef936f9b5a169bc7f35ed2d151d360925e
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-04-11 12:46:42 -0500

    COMMIT TABLA NUEVOS USUARIOS

--- AdminWeb/public/dashboard/PanelNuevosUsuarios.js
+++ AdminWeb/public/dashboard/PanelNuevosUsuarios.js
@@ -26,7 +26,7 @@ function FillTable(){
 	socket.on('NotificationNewUser', function(data){
 		$("#NewUsersTable > tbody").html("");
 		for (var i = 0; i < data.length; i++) {
-				$('#NewUsersTable').append("<tbody><tr><td>"+data[i].person.PersonCi+"</td><td>"+data[i].person.PersonName+' '+data[i].person.PersonLastName+"</td><td>"+data[i].person.PersonAddress+"</td><td>"+data[i].person.PersonPhone+"</td><td>"+data[i].person.PersonRuc+"</td><td>"+data[i].person.PersonRole+"</td><td>"+data[i].user.UserEmail+"</td><td>"+data[i].user.UserProfile+
+				$('#NewUsersTable').append("<tbody><tr><td>"+data[i].person.PERSONCIRUC+"</td><td>"+data[i].person.PERSONNAME+' '+data[i].person.PERSONLASTNAME+"</td><td>"+data[i].person.PERSONADDRESS+"</td><td>"+data[i].person.PERSONPHONE+"</td><td>"+data[i].person.PERSONROLE+"</td><td>"+data[i].user.USEREMAIL+"</td><td>"+data[i].user.USERPROFILE+
        							"</td><td><button class='btn green' onclick='SaveNewUser("+i+")'><i class='fa fa-save'></i>  Guardar</button></td></tr></tbody>");
 			}
 		lstObjNewUsers=data;

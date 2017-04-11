9786e22857863da65aac270691578e18a7287543
AdrMenaM <alex_m6m@hotmail.com>
2017-04-11 14:08:34 -0500

    Correcciones Emergency Notification

--- AdminWeb/public/dashboard/GeneralScripts.js
+++ AdminWeb/public/dashboard/GeneralScripts.js
@@ -2,6 +2,7 @@ var socket = io.connect("http://34.195.35.232:8080",{"forceNew": true});
 var lstNewUsers=[];
 $(document).ready(function(){
 })
+
 socket.on('NotificationNewUser', function(data){
 	lstNewUsers=[];
 	lstNewUsers=data;
@@ -10,10 +11,10 @@ socket.on('NotificationNewUser', function(data){
 	if(lstNewUsers.length!=0){
 		$.notific8('Nueva solicitud de usuario recibida');
 	}
-});
+})
 
 socket.on('EmergencyNotification', function(msg){
-		console.log(msg);
+	console.log(msg);
 	$.notific8(msg);
 
 });

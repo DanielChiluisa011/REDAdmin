933b270e7f91b6c35662b332dfe1db864bd972bc
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-04-11 13:05:42 -0500

    commit QUERY NUEVOS USUARIOS

--- AdminWeb/public/dashboard/PanelNuevosUsuarios.js
+++ AdminWeb/public/dashboard/PanelNuevosUsuarios.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 })
 
 function SaveNewUser(i){
-	bootbox.confirm("¿Esta seguro de guardar el usuario "+lstObjNewUsers[i].user.UserEmail+"?", function(result) {
+	bootbox.confirm("¿Esta seguro de guardar el usuario "+lstObjNewUsers[i].user.USEREMAIL+"?", function(result) {
 	   if(result){
 		   	socket.emit('SaveNewUser', lstObjNewUsers[i]);
 			$.notific8('Usuario guardado correctamente');
@@ -26,7 +26,7 @@ function FillTable(){
 	socket.on('NotificationNewUser', function(data){
 		$("#NewUsersTable > tbody").html("");
 		for (var i = 0; i < data.length; i++) {
-				$('#NewUsersTable').append("<tbody><tr><td>"+data[i].person.PERSONCIRUC+"</td><td>"+data[i].person.PERSONNAME+' '+data[i].person.PERSONLASTNAME+"</td><td>"+data[i].person.PERSONADDRESS+"</td><td>"+data[i].person.PERSONPHONE+"</td><td>"+data[i].person.PERSONROLE+"</td><td>"+data[i].user.USEREMAIL+"</td><td>"+data[i].user.USERPROFILE+
+				$('#NewUsersTable').append("<tbody><tr><td>"+data[i].person.PERSONCI+"</td><td>"+data[i].person.PERSONNAME+' '+data[i].person.PERSONLASTNAME+"</td><td>"+data[i].person.PERSONADDRESS+"</td><td>"+data[i].person.PERSONPHONE+"</td><td>"+data[i].person.PERSONROLE+"</td><td>"+data[i].user.USEREMAIL+"</td><td>"+data[i].user.USERPROFILE+
        							"</td><td><button class='btn green' onclick='SaveNewUser("+i+")'><i class='fa fa-save'></i>  Guardar</button></td></tr></tbody>");
 			}
 		lstObjNewUsers=data;

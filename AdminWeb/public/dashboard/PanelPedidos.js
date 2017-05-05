a5cebba761ba42dfea761360349a3353b5b96e2d
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-05 10:20:03 -0500

    cambio panelpedidos.js

--- AdminWeb/public/dashboard/PanelPedidos.js
+++ AdminWeb/public/dashboard/PanelPedidos.js
@@ -616,7 +616,7 @@ $('#btnSaveJourney').click(function(){
 		   			JourneyRoute+=',';	
 		   		}
 		   	}
-			   console.log(lstRecyclingCenters[$('#cmbRecyclingCenters option:selected').index()-1]);
+			console.log("cr "+lstRecyclingCenters[$('#cmbRecyclingCenters option:selected').index()-1]);
 	   		var journey = {
 	   			date: $('#txtNewJourneyDate').val(),
 	   			state: 'Pendiente',

9574773665c6fc735ce2d34c8f12cf2b74578082
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-15 12:56:20 -0500

    prueba

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -145,7 +145,7 @@ $(document).ready(function(data){
 						console.log(lstUsers[g].person.PERSONID+"  "+lstTrucks[i].PERSONID)
 						if(lstUsers[g].person.PERSONID==lstTrucks[i].PERSONID){
 							console.log("encontro");
-							driver=lstUsers[i];
+							driver=lstUsers[g].person;
 
 							console.log(driver);
 						}

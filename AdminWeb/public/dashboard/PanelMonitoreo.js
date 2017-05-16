1065ed5787b068d37a6d4bda6e7a4a11d627e949
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-16 11:03:06 -0500

    prueba

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -72,7 +72,7 @@ $(document).ready(function(data){
 			socket.emit("RequestDriver",lstJourneys[j].truckId);
 			socket.on("ResponseDriver",function(data){
 				driver=data;
-				console.log(driver);
+				console.log(lstJourneys.length);
 				$('#ActiveOrders').append("<tbody><tr><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowJourney("+j+")'>"+
 									lstJourneys[j].JourneyDate+"</td><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowJourney("+j+")'>"+ +"</td><td onclick='ShowJourney("+j+")'>"+RCName+
 									"</td><td onclick='ShowJourney("+j+")'>"+importerName+"</td><td><a class='btn red btn-outline sbold' data-toggle='modal' href='' onclick='CurrentDate()'> <i class='fa fa-close'> </i> Suspender </a></td></tr><tbody>");  

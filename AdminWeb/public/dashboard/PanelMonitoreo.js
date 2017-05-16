258e894bf058300c89488f1722fbc0be08703172
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-16 10:18:00 -0500

    PRUEBA

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -74,10 +74,13 @@ $(document).ready(function(data){
 				driver=data;
 				console.log(i);
 				console.log(driver);
-			});
-			$('#ActiveOrders').append("<tbody><tr><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowJourney("+j+")'>"+
+				$('#ActiveOrders').append("<tbody><tr><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowJourney("+j+")'>"+
 									lstJourneys[j].JourneyDate+"</td><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowJourney("+j+")'>"+ driver.PERSONNAME +" "+driver.PERSONLASTNAME+"</td><td onclick='ShowJourney("+j+")'>"+RCName+
 									"</td><td onclick='ShowJourney("+j+")'>"+importerName+"</td><td><a class='btn red btn-outline sbold' data-toggle='modal' href='' onclick='CurrentDate()'> <i class='fa fa-close'> </i> Suspender </a></td></tr><tbody>");  
+			});
+			// $('#ActiveOrders').append("<tbody><tr><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowJourney("+j+")'>"+
+			// 						lstJourneys[j].JourneyDate+"</td><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowJourney("+j+")'>"+ driver.PERSONNAME +" "+driver.PERSONLASTNAME+"</td><td onclick='ShowJourney("+j+")'>"+RCName+
+			// 						"</td><td onclick='ShowJourney("+j+")'>"+importerName+"</td><td><a class='btn red btn-outline sbold' data-toggle='modal' href='' onclick='CurrentDate()'> <i class='fa fa-close'> </i> Suspender </a></td></tr><tbody>");  
 		}
     }) 	
 

79dd8d5b50fca0d49db2f1e4f26de950e4dfe008
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-16 11:24:57 -0500

    prueba

--- AdminWeb/server/main.js
+++ AdminWeb/server/main.js
@@ -348,8 +348,8 @@ io.on('connection', function(socket){
 	SaveNewUser(socket);
 
 	socket.on("RequestDriver",function(obj){
-		console.log("RequestTrucks: "+truckId+" indx"+obj.indx);
-		connection.query("SELECT PERSONID, PERSONNAME,PERSONLASTNAME,PERSONPHONE FROM person WHERE PERSONID =  (SELECT PERSONID FROM trucks WHERE TRUCKID='"+obj.truckId+"');",function(error, result){
+		console.log("RequestTrucks: "+id+" indx"+obj.indx);
+		connection.query("SELECT PERSONID, PERSONNAME,PERSONLASTNAME,PERSONPHONE FROM person WHERE PERSONID =  (SELECT PERSONID FROM trucks WHERE TRUCKID='"+obj.id+"');",function(error, result){
 			if(error){
 				throw error;
 			}else{

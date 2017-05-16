05d647fd11f8655d8cd5ec6a4e4726cc322d534d
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-16 09:47:36 -0500

    Vuelta Inicial

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -110,7 +110,7 @@ $(document).ready(function(data){
 					}
 				}
 				$('#ActiveOrders').append("<tbody><tr><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowJourney("+j+")'>"+
-										lstJourneys[j].JourneyDate+"</td><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowJourney("+j+")'>"+ lstUsers[driverS].PERSONNAME+" "+lstUsers[driverS].PERSONLASTNAME +"</td><td onclick='ShowJourney("+j+")'>"+RCName+
+										lstJourneys[j].JourneyDate+"</td><td onclick='ShowJourney("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowJourney("+j+")'>"+  +"</td><td onclick='ShowJourney("+j+")'>"+RCName+
 										"</td><td onclick='ShowJourney("+j+")'>"+importerName+"</td><td><a class='btn red btn-outline sbold' data-toggle='modal' href='' onclick='CurrentDate()'> <i class='fa fa-close'> </i> Suspender </a></td></tr><tbody>");  
 			}
 		// })

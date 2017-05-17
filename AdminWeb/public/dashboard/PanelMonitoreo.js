cde066b55e24cddfc4cdb1377cadb8bfe4730742
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-17 07:55:06 -0500

    prueba
    socket.removeAllListeners(); en showJourney

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -56,7 +56,7 @@ $(document).ready(function(data){
        	lstOrders=[];
        	lstOrders=data;
 		for ( var j = 0; j <lstJourneys.length; j++) {
-			socket.removeAllListeners();
+			// socket.removeAllListeners();
 			for (var i = 0; i < lstImporters.length; i++) {
 				if(lstJourneys[j].ImporterId==lstImporters[i].IMPORTERID){
 					importerName=lstImporters[i].IMPORTERNAME;
@@ -134,7 +134,7 @@ var MapsGoogle = function () {
     };
 }();
 function ShowJourney(i){
-	
+	socket.removeAllListeners();
 	var ObjJourney;
 	jQuery(document).ready(function() {
 	    MapsGoogle.init();

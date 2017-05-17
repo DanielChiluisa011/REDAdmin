09ad5fbb0d07dc7c7a0b742596aa9d09f05e7911
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-17 09:04:16 -0500

    cambio formato fecha

--- AdminWeb/public/dashboard/PanelPedidos.js
+++ AdminWeb/public/dashboard/PanelPedidos.js
@@ -693,8 +693,5 @@ function CurrentDate(){
 	var d = new Date();
 	var month = d.getMonth()+1;
 	var day = d.getDate();
-	var output = (day<10 ? '0' : '') + day + '-' +
-    (month<10 ? '0' : '') + month + '-' +
-    d.getFullYear()
-    return output;
+	var output =  d.getFullYear()+'-'+(month<10 ? '0' : '') + month + '-' + (day<10 ? '0' : '') + day
 }

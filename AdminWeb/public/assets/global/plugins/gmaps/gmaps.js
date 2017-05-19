cc531c28e7c120e8c1a88580968ebe9b192c95ae
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-05-19 12:57:58 -0500

    // optimizeWaypoints: true,

--- AdminWeb/public/assets/global/plugins/gmaps/gmaps.js
+++ AdminWeb/public/assets/global/plugins/gmaps/gmaps.js
@@ -1402,7 +1402,6 @@ GMaps.prototype.travelRoute = function(options) {
       destination: options.destination,
       travelMode: options.travelMode,
       waypoints : options.waypoints,
-      // optimizeWaypoints: true,
       unitSystem: options.unitSystem,
       error: options.error,
       callback: function(e) {

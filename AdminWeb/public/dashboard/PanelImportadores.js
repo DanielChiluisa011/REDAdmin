7aacaee23af847f7da57a323cbd74b45bef0c5e9
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-06-06 11:54:39 -0500

    Update

--- AdminWeb/public/dashboard/PanelImportadores.js
+++ AdminWeb/public/dashboard/PanelImportadores.js
@@ -97,96 +97,96 @@ function ShowImporterInformation(i){
         $("#txtNewImpEmail").attr("disabled",true);
 }
 $("#btnInsertImporter").click(function(){
-    console.log($("#btnInsertImporter").html());
-    // if($("#btnInsertImporter").attr("value")=="Guardar"){
-    //     bootbox.confirm("¿Desea guardar la información ingresada? ", function(result) {
-    //         if(result){
-    //             var newImporter = {
-    //                 name: $("#txtNewImpName").val(),
-    //                 address: $("#txtNewImpAddress").val(),
-    //                 phone: $("#txtNewImpPhone").val(),
-    //                 rucImporter: $("#txtNewImpRuc").val(),
-    //                 quota: $("#txtNewImpQuota").val(),
-    //                 licence: $("#txtNewImpLicence").val(),
-    //                 personName: $("#txtNewImpPersonName").val(),
-    //                 personLastName: $("#txtNewImpPersonLastName").val(),
-    //                 personCi: $("#txtNewImpPersonId").val(),
-    //                 personPhone: $("#txtNewImpPersonPhone").val(),
-    //                 personAddress: $("#txtNewImpPersonAddress").val(),
-    //                 personEmail: $("#txtNewImpEmail").val(),
-    //             } 
-    //             console.log("NUEVO IMPORTADOR");
-    //             console.log(newImporter);
-    //             socket.emit("RequestInsertNewImporter",newImporter);
-    //             socket.on("ResponseImporter",function(flag){
-    //                 if(flag){
-    //                     $.notific8('Datos guardados correctamente', {
-    //                         life: 3500,
-    //                         heading: 'Listo!',
-    //                         theme: 'teal',
-    //                         sticky: false,
-    //                         horizontalEdge: 'top',
-    //                         verticalEdge: 'rigth',
-    //                         zindex: 1500
-    //                         });
-    //                 }else{
-    //                     $.notific8('Error al guardar, intentelo nuevamente', {
-    //                         life: 3500,
-    //                         heading: 'Error!',
-    //                         theme: 'ruby',
-    //                         sticky: false,
-    //                         horizontalEdge: 'top',
-    //                         verticalEdge: 'rigth',
-    //                         zindex: 1500
-    //                     });
-    //                 }
-    //             });
-    //         }
-    //     });
-    // }else{
-    //     bootbox.confirm("¿Desea actualizar la información? ", function(result) {
-    //         if(result){
-    //             var newImporter = {
-    //                 name: $("#txtNewImpName").val(),
-    //                 address: $("#txtNewImpAddress").val(),
-    //                 phone: $("#txtNewImpPhone").val(),
-    //                 rucImporter: $("#txtNewImpRuc").val(),
-    //                 quota: $("#txtNewImpQuota").val(),
-    //                 licence: $("#txtNewImpLicence").val(),
-    //                 personName: $("#txtNewImpPersonName").val(),
-    //                 personLastName: $("#txtNewImpPersonLastName").val(),
-    //                 personCi: $("#txtNewImpPersonId").val(),
-    //                 personPhone: $("#txtNewImpPersonPhone").val(),
-    //                 personAddress: $("#txtNewImpPersonAddress").val(),
-    //                 personEmail: $("#txtNewImpEmail").val(),
-    //             } 
-    //             socket.emit("RequestUpdateImporter",newImporter);
-    //             socket.on("RequestErrorUpdateImporter",function(flag){
-    //                 if(flag){
-    //                     $.notific8('Datos actualizados correctamente', {
-    //                         life: 3500,
-    //                         heading: 'Listo!',
-    //                         theme: 'teal',
-    //                         sticky: false,
-    //                         horizontalEdge: 'top',
-    //                         verticalEdge: 'rigth',
-    //                         zindex: 1500
-    //                         });
-    //                 }else{
-    //                     $.notific8('Error al guardar, intentelo nuevamente', {
-    //                         life: 3500,
-    //                         heading: 'Error!',
-    //                         theme: 'ruby',
-    //                         sticky: false,
-    //                         horizontalEdge: 'top',
-    //                         verticalEdge: 'rigth',
-    //                         zindex: 1500
-    //                     });
-    //                 }
-    //             });
-    //         }
-    //     });
-    // }
+    // console.log($("#btnInsertImporter").html());
+    if($("#btnInsertImporter").html()=="Guardar"){
+        bootbox.confirm("¿Desea guardar la información ingresada? ", function(result) {
+            if(result){
+                var newImporter = {
+                    name: $("#txtNewImpName").val(),
+                    address: $("#txtNewImpAddress").val(),
+                    phone: $("#txtNewImpPhone").val(),
+                    rucImporter: $("#txtNewImpRuc").val(),
+                    quota: $("#txtNewImpQuota").val(),
+                    licence: $("#txtNewImpLicence").val(),
+                    personName: $("#txtNewImpPersonName").val(),
+                    personLastName: $("#txtNewImpPersonLastName").val(),
+                    personCi: $("#txtNewImpPersonId").val(),
+                    personPhone: $("#txtNewImpPersonPhone").val(),
+                    personAddress: $("#txtNewImpPersonAddress").val(),
+                    personEmail: $("#txtNewImpEmail").val(),
+                } 
+                console.log("NUEVO IMPORTADOR");
+                console.log(newImporter);
+                socket.emit("RequestInsertNewImporter",newImporter);
+                socket.on("ResponseImporter",function(flag){
+                    if(flag){
+                        $.notific8('Datos guardados correctamente', {
+                            life: 3500,
+                            heading: 'Listo!',
+                            theme: 'teal',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                            });
+                    }else{
+                        $.notific8('Error al guardar, intentelo nuevamente', {
+                            life: 3500,
+                            heading: 'Error!',
+                            theme: 'ruby',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                    }
+                });
+            }
+        });
+    }else{
+        bootbox.confirm("¿Desea actualizar la información? ", function(result) {
+            if(result){
+                var newImporter = {
+                    name: $("#txtNewImpName").val(),
+                    address: $("#txtNewImpAddress").val(),
+                    phone: $("#txtNewImpPhone").val(),
+                    rucImporter: $("#txtNewImpRuc").val(),
+                    quota: $("#txtNewImpQuota").val(),
+                    licence: $("#txtNewImpLicence").val(),
+                    personName: $("#txtNewImpPersonName").val(),
+                    personLastName: $("#txtNewImpPersonLastName").val(),
+                    personCi: $("#txtNewImpPersonId").val(),
+                    personPhone: $("#txtNewImpPersonPhone").val(),
+                    personAddress: $("#txtNewImpPersonAddress").val(),
+                    personEmail: $("#txtNewImpEmail").val(),
+                } 
+                socket.emit("RequestUpdateImporter",newImporter);
+                socket.on("RequestErrorUpdateImporter",function(flag){
+                    if(flag){
+                        $.notific8('Datos actualizados correctamente', {
+                            life: 3500,
+                            heading: 'Listo!',
+                            theme: 'teal',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                            });
+                    }else{
+                        $.notific8('Error al guardar, intentelo nuevamente', {
+                            life: 3500,
+                            heading: 'Error!',
+                            theme: 'ruby',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                    }
+                });
+            }
+        });
+    }
 	
 	
 	

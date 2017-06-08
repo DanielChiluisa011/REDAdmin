ad29b9283915d0913f6ea47bbbd7f7b23b46dee7
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-06-08 08:43:04 -0500

    Creación Pestañas Camiones
    Inti Guzman

---
+++ AdminWeb/public/dashboard/PanelCamiones.js
@@ -0,0 +1,176 @@
+var socket = io.connect("http://34.195.35.232:8080",{"forceNew": true});
+var lstCR= [];
+var mapa;
+var MapsGoogle = function () {
+
+    var mapBasic = function () {
+        mapa=new GMaps({
+            div: '#MapIngreso',
+            lat: -0.191611,
+            lng:  -78.483574
+        });
+         mapa.setZoom(10);
+		 
+    }
+    return {
+        //main function to initiate map samples
+        init: function () {
+            mapBasic();
+        }
+    };
+}();
+$(document).ready(function(){
+     MapsGoogle.init();
+    socket.emit("RequestCRInfo","");
+    socket.on("ResponseCRInfo",function(flag){
+        if(flag==0){
+            $.notific8('Error al cargar, intentelo nuevamente', {
+                life: 3500,
+                heading: 'Error!',
+                theme: 'ruby',
+                sticky: false,
+                horizontalEdge: 'top',
+                verticalEdge: 'rigth',
+                zindex: 1500
+            });
+        }
+    });
+    socket.on("ResponseCR",function(CR){
+        lstCR.length=0;
+        lstCR=CR;
+        $("#txtNewImpName").val("");
+		$("#txtNewImpAddress").val("");
+		$("#txtNewImpPhone").val("");
+		$("#txtNewImpRuc").val("");
+		$("#txtNewImpQuota").val("");
+		$("#txtNewImpLicence").val("");
+		$("#txtNewImpPersonName").val("");
+		$("#txtNewImpPersonLastName").val("");
+		$("#txtNewImpPersonId").val("");
+		$("#txtNewImpPersonPhone").val("");
+		$("#txtNewImpPersonAddress").val("");
+		$("#txtNewImpEmail").val("");
+        $("#ImportersTable > tbody").html("");
+        for (var i = 0; i < CR.length; i++) {
+            $('#ImportersTable').append("<tbody>"+
+                                        "<tr>"+
+                                            "<td>"+CR[i].RECYCLINGCENTERNAME+"</td>"+
+                                            "<td>"+CR[i].RECYCLINGCENTERADDRESS+"</td>"+
+                                            "<td>"+CR[i].RECYCLINGCENTERPHONE+"</td>"+
+                                            "<td>"+CR[i].PERSONNAME+' '+CR[i].PERSONLASTNAME+"</td>"+
+                                            //"<td><a class='btn red btn-outline sbold' data-toggle='modal' href='#addImporter' onclick='ShowImporterInformation("+i+")'> <i class='fa fa-edit'> </i> Editar </a></td>"+
+                                        "</tr>"+
+                                    "</tbody>");
+        }
+    });
+});
+
+$("#btnCancelImporter").click(function(){
+        $("#btnInsertImporter").html('Guardar');
+        $("#txtNewImpName").val(""),
+		$("#txtNewImpAddress").val("");
+		$("#txtNewImpPhone").val("");
+		$("#txtNewImpPersonName").val("");
+		$("#txtNewImpPersonLastName").val("");
+		$("#txtNewImpPersonId").val("");
+		$("#txtNewImpPersonPhone").val("");
+		$("#txtNewImpPersonAddress").val("");
+
+        $("#txtNewImpName").attr("disabled",false);
+        $("#txtNewImpPersonName").attr("disabled",false);
+        $("#txtNewImpPersonLastName").attr("disabled",false);
+        $("#txtNewImpPersonId").attr("disabled",false);
+});
+function ShowImporterInformation(i){
+        $("#btnInsertImporter").html('Actualizar');
+        $("#txtNewImpName").attr("disabled",true);
+        $("#txtNewImpName").val(lstCR[i].RECYCLINGCENTERNAME);
+		$("#txtNewImpAddress").val(lstCR[i].RECYCLINGCENTERADDRESS);
+		$("#txtNewImpPhone").val(lstCR[i].RECYCLINGCENTERPHONE);
+		$("#txtNewImpPersonName").val(lstCR[i].PERSONNAME);
+        $("#txtNewImpPersonName").attr("disabled",true);
+		$("#txtNewImpPersonLastName").val(lstCR[i].PERSONLASTNAME);
+        $("#txtNewImpPersonLastName").attr("disabled",true);
+		$("#txtNewImpPersonId").val(lstCR[i].PERSONCIRUC);
+        $("#txtNewImpPersonId").attr("disabled",true);
+		$("#txtNewImpPersonPhone").val(lstCR[i].PERSONPHONE);
+		$("#txtNewImpPersonAddress").val(lstCR[i].PERSONADDRESS);
+}
+$("#btnInsertImporter").click(function(){
+    var newCR = {
+        name: $("#txtNewImpName").val(),
+        address: $("#txtNewImpAddress").val(),
+        phone: $("#txtNewImpPhone").val(),
+        personName: $("#txtNewImpPersonName").val(),
+        personLastName: $("#txtNewImpPersonLastName").val(),
+        personCi: $("#txtNewImpPersonId").val(),
+        personPhone: $("#txtNewImpPersonPhone").val(),
+        personAddress: $("#txtNewImpPersonAddress").val()
+    } 
+    console.log("NUEVO CR");
+    console.log(newCR);
+    if($("#btnInsertImporter").html()=="Guardar"){
+        bootbox.confirm("¿Desea guardar la información ingresada? ", function(result) {
+            if(result){
+                socket.emit("RequestInsertnewCR",newCR);
+                socket.on("ResponseImporter",function(flag){
+                    if(flag){
+                        $.notific8('Datos guardados correctamente', {
+                            life: 3500,
+                            heading: 'Listo!',
+                            theme: 'teal',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                        location.reload();	
+                    }else{
+                        $.notific8('Error al guardar, intentelo nuevamente', {
+                            life: 3500,
+                            heading: 'Error!',
+                            theme: 'ruby',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                    }
+                });
+            }
+        });
+    }else{
+        bootbox.confirm("¿Desea actualizar la información? ", function(result) {
+            if(result){
+                socket.emit("RequestUpdateImporter",newCR);
+                socket.on("RequestErrorUpdateImporter",function(flag){
+                    if(flag){
+                        $.notific8('Datos actualizados correctamente', {
+                            life: 3500,
+                            heading: 'Listo!',
+                            theme: 'teal',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                        location.reload();	
+                    }else{
+                        $.notific8('Error al guardar, intentelo nuevamente', {
+                            life: 3500,
+                            heading: 'Error!',
+                            theme: 'ruby',
+                            sticky: false,
+                            horizontalEdge: 'top',
+                            verticalEdge: 'rigth',
+                            zindex: 1500
+                        });
+                    }
+                });
+            }
+        });
+    }
+	
+	
+	
+});

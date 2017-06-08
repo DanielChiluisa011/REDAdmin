eb610b3f4c6182d793d127bc0b7d446a5187d1ad
DanielChiluisa011 <amir@Amirs-MacBook-Pro.local>
2017-06-08 12:36:17 -0500

    Creación lista conductores Camion
    Inti Guzman

--- AdminWeb/public/dashboard/PanelCamiones.js
+++ AdminWeb/public/dashboard/PanelCamiones.js
@@ -1,5 +1,6 @@
 var socket = io.connect("http://34.195.35.232:8080",{"forceNew": true});
 var lstTrucks= [];
+var lstDrivers=[];
 var mapa;
 var MapsGoogle = function () {
 
@@ -38,18 +39,19 @@ $(document).ready(function(){
     socket.on("SelectTrucks1",function(CR){
         lstTrucks.length=0;
         lstTrucks=CR;
-        $("#txtNewImpName").val("");
-		$("#txtNewImpAddress").val("");
-		$("#txtNewImpPhone").val("");
-		$("#txtNewImpRuc").val("");
-		$("#txtNewImpQuota").val("");
-		$("#txtNewImpLicence").val("");
-		$("#txtNewImpPersonName").val("");
-		$("#txtNewImpPersonLastName").val("");
-		$("#txtNewImpPersonId").val("");
-		$("#txtNewImpPersonPhone").val("");
-		$("#txtNewImpPersonAddress").val("");
-		$("#txtNewImpEmail").val("");
+        $("#txtNewTruckId").val("");
+		$("#txtNewTruckModel").val("");
+		$("#txtNewTruckSize").val("");
+		$("#txtNewTruckTradeMark").val("");
+        
+		// $("#txtNewImpQuota").val("");
+		// $("#txtNewImpLicence").val("");
+		// $("#txtNewImpPersonName").val("");
+		// $("#txtNewImpPersonLastName").val("");
+		// $("#txtNewImpPersonId").val("");
+		// $("#txtNewImpPersonPhone").val("");
+		// $("#txtNewImpPersonAddress").val("");
+		// $("#txtNewImpEmail").val("");
         $("#ImportersTable > tbody").html("");
         for (var i = 0; i < CR.length; i++) {
             $('#ImportersTable').append("<tbody>"+
@@ -65,114 +67,127 @@ $(document).ready(function(){
                                     "</tbody>");
         }
     });
+    socket.on('SelectDrivers', function(data){
+		// lstDrivers=[];
+       	lstDrivers=data;
+       	$('#cmbNewTruckDriver').empty();
+       	$('#cmbNewTruckDriver').append('<option selected>Seleccione un Conductor</option>');
+       	for (var i = 0; i < lstDrivers.length; i++) {
+	   		$('#cmbNewTruckDriver').append(new Option(lstDrivers[i].PERSONNAME+" "+lstDrivers[i].PERSONLASTNAME, 'names'));
+	   	}
+
+   	})
 });
 
-$("#btnCancelImporter").click(function(){
-        $("#btnInsertImporter").html('Guardar');
-        $("#txtNewImpName").val(""),
-		$("#txtNewImpAddress").val("");
-		$("#txtNewImpPhone").val("");
-		$("#txtNewImpPersonName").val("");
-		$("#txtNewImpPersonLastName").val("");
-		$("#txtNewImpPersonId").val("");
-		$("#txtNewImpPersonPhone").val("");
-		$("#txtNewImpPersonAddress").val("");
 
-        $("#txtNewImpName").attr("disabled",false);
-        $("#txtNewImpPersonName").attr("disabled",false);
-        $("#txtNewImpPersonLastName").attr("disabled",false);
-        $("#txtNewImpPersonId").attr("disabled",false);
-});
-function ShowImporterInformation(i){
-        $("#btnInsertImporter").html('Actualizar');
-        $("#txtNewImpName").attr("disabled",true);
-        $("#txtNewImpName").val(lstCR[i].RECYCLINGCENTERNAME);
-		$("#txtNewImpAddress").val(lstCR[i].RECYCLINGCENTERADDRESS);
-		$("#txtNewImpPhone").val(lstCR[i].RECYCLINGCENTERPHONE);
-		$("#txtNewImpPersonName").val(lstCR[i].PERSONNAME);
-        $("#txtNewImpPersonName").attr("disabled",true);
-		$("#txtNewImpPersonLastName").val(lstCR[i].PERSONLASTNAME);
-        $("#txtNewImpPersonLastName").attr("disabled",true);
-		$("#txtNewImpPersonId").val(lstCR[i].PERSONCIRUC);
-        $("#txtNewImpPersonId").attr("disabled",true);
-		$("#txtNewImpPersonPhone").val(lstCR[i].PERSONPHONE);
-		$("#txtNewImpPersonAddress").val(lstCR[i].PERSONADDRESS);
-}
-$("#btnInsertImporter").click(function(){
-    var newCR = {
-        name: $("#txtNewImpName").val(),
-        address: $("#txtNewImpAddress").val(),
-        phone: $("#txtNewImpPhone").val(),
-        personName: $("#txtNewImpPersonName").val(),
-        personLastName: $("#txtNewImpPersonLastName").val(),
-        personCi: $("#txtNewImpPersonId").val(),
-        personPhone: $("#txtNewImpPersonPhone").val(),
-        personAddress: $("#txtNewImpPersonAddress").val()
-    } 
-    console.log("NUEVO CR");
-    console.log(newCR);
-    if($("#btnInsertImporter").html()=="Guardar"){
-        bootbox.confirm("¿Desea guardar la información ingresada? ", function(result) {
-            if(result){
-                socket.emit("RequestInsertnewCR",newCR);
-                socket.on("ResponseImporter",function(flag){
-                    if(flag){
-                        $.notific8('Datos guardados correctamente', {
-                            life: 3500,
-                            heading: 'Listo!',
-                            theme: 'teal',
-                            sticky: false,
-                            horizontalEdge: 'top',
-                            verticalEdge: 'rigth',
-                            zindex: 1500
-                        });
-                        location.reload();	
-                    }else{
-                        $.notific8('Error al guardar, intentelo nuevamente', {
-                            life: 3500,
-                            heading: 'Error!',
-                            theme: 'ruby',
-                            sticky: false,
-                            horizontalEdge: 'top',
-                            verticalEdge: 'rigth',
-                            zindex: 1500
-                        });
-                    }
-                });
-            }
-        });
-    }else{
-        bootbox.confirm("¿Desea actualizar la información? ", function(result) {
-            if(result){
-                socket.emit("RequestUpdateImporter",newCR);
-                socket.on("RequestErrorUpdateImporter",function(flag){
-                    if(flag){
-                        $.notific8('Datos actualizados correctamente', {
-                            life: 3500,
-                            heading: 'Listo!',
-                            theme: 'teal',
-                            sticky: false,
-                            horizontalEdge: 'top',
-                            verticalEdge: 'rigth',
-                            zindex: 1500
-                        });
-                        location.reload();	
-                    }else{
-                        $.notific8('Error al guardar, intentelo nuevamente', {
-                            life: 3500,
-                            heading: 'Error!',
-                            theme: 'ruby',
-                            sticky: false,
-                            horizontalEdge: 'top',
-                            verticalEdge: 'rigth',
-                            zindex: 1500
-                        });
-                    }
-                });
-            }
-        });
-    }
+
+
+// $("#btnCancelImporter").click(function(){
+//         $("#btnInsertImporter").html('Guardar');
+//         $("#txtNewImpName").val(""),
+// 		$("#txtNewImpAddress").val("");
+// 		$("#txtNewImpPhone").val("");
+// 		$("#txtNewImpPersonName").val("");
+// 		$("#txtNewImpPersonLastName").val("");
+// 		$("#txtNewImpPersonId").val("");
+// 		$("#txtNewImpPersonPhone").val("");
+// 		$("#txtNewImpPersonAddress").val("");
+
+//         $("#txtNewImpName").attr("disabled",false);
+//         $("#txtNewImpPersonName").attr("disabled",false);
+//         $("#txtNewImpPersonLastName").attr("disabled",false);
+//         $("#txtNewImpPersonId").attr("disabled",false);
+// });
+// function ShowImporterInformation(i){
+//         $("#btnInsertImporter").html('Actualizar');
+//         $("#txtNewImpName").attr("disabled",true);
+//         $("#txtNewImpName").val(lstCR[i].RECYCLINGCENTERNAME);
+// 		$("#txtNewImpAddress").val(lstCR[i].RECYCLINGCENTERADDRESS);
+// 		$("#txtNewImpPhone").val(lstCR[i].RECYCLINGCENTERPHONE);
+// 		$("#txtNewImpPersonName").val(lstCR[i].PERSONNAME);
+//         $("#txtNewImpPersonName").attr("disabled",true);
+// 		$("#txtNewImpPersonLastName").val(lstCR[i].PERSONLASTNAME);
+//         $("#txtNewImpPersonLastName").attr("disabled",true);
+// 		$("#txtNewImpPersonId").val(lstCR[i].PERSONCIRUC);
+//         $("#txtNewImpPersonId").attr("disabled",true);
+// 		$("#txtNewImpPersonPhone").val(lstCR[i].PERSONPHONE);
+// 		$("#txtNewImpPersonAddress").val(lstCR[i].PERSONADDRESS);
+// }
+// $("#btnInsertImporter").click(function(){
+//     var newCR = {
+//         name: $("#txtNewImpName").val(),
+//         address: $("#txtNewImpAddress").val(),
+//         phone: $("#txtNewImpPhone").val(),
+//         personName: $("#txtNewImpPersonName").val(),
+//         personLastName: $("#txtNewImpPersonLastName").val(),
+//         personCi: $("#txtNewImpPersonId").val(),
+//         personPhone: $("#txtNewImpPersonPhone").val(),
+//         personAddress: $("#txtNewImpPersonAddress").val()
+//     } 
+//     console.log("NUEVO CR");
+//     console.log(newCR);
+//     if($("#btnInsertImporter").html()=="Guardar"){
+//         bootbox.confirm("¿Desea guardar la información ingresada? ", function(result) {
+//             if(result){
+//                 socket.emit("RequestInsertnewCR",newCR);
+//                 socket.on("ResponseImporter",function(flag){
+//                     if(flag){
+//                         $.notific8('Datos guardados correctamente', {
+//                             life: 3500,
+//                             heading: 'Listo!',
+//                             theme: 'teal',
+//                             sticky: false,
+//                             horizontalEdge: 'top',
+//                             verticalEdge: 'rigth',
+//                             zindex: 1500
+//                         });
+//                         location.reload();	
+//                     }else{
+//                         $.notific8('Error al guardar, intentelo nuevamente', {
+//                             life: 3500,
+//                             heading: 'Error!',
+//                             theme: 'ruby',
+//                             sticky: false,
+//                             horizontalEdge: 'top',
+//                             verticalEdge: 'rigth',
+//                             zindex: 1500
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }else{
+//         bootbox.confirm("¿Desea actualizar la información? ", function(result) {
+//             if(result){
+//                 socket.emit("RequestUpdateImporter",newCR);
+//                 socket.on("RequestErrorUpdateImporter",function(flag){
+//                     if(flag){
+//                         $.notific8('Datos actualizados correctamente', {
+//                             life: 3500,
+//                             heading: 'Listo!',
+//                             theme: 'teal',
+//                             sticky: false,
+//                             horizontalEdge: 'top',
+//                             verticalEdge: 'rigth',
+//                             zindex: 1500
+//                         });
+//                         location.reload();	
+//                     }else{
+//                         $.notific8('Error al guardar, intentelo nuevamente', {
+//                             life: 3500,
+//                             heading: 'Error!',
+//                             theme: 'ruby',
+//                             sticky: false,
+//                             horizontalEdge: 'top',
+//                             verticalEdge: 'rigth',
+//                             zindex: 1500
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }
 	
 	
 	
-});
+// });

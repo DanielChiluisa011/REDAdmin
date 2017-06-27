869d3e54acba572c6eb8ca22c434487f435bc0ea
TTI-DanielChiluisa <daniel@taotechideas.com>
2017-06-27 11:33:36 -0500

    F_ShowRouteTest_05
    Change i by j on RouteItem.push

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -169,7 +169,7 @@ function ShowRouteTest(i){
 	for(var k=0;k<RouteSelected.length;k++){
 		for (var j = 0; j < AuxlstOrders.length; j++) {
  					if(AuxlstOrders[j].OrderState=="En Proceso"){
-						RouteItem.push(lstOrders[i]);	
+						RouteItem.push(AuxlstOrders[j]);	
 					}
 					if(RouteSelected[k].DistributorId==AuxlstOrders[j].DistributorId){
  						var aux=AuxlstOrders[k].OrderQuantity

e6c879412dcbe6b23770092f6e482f6bc920c06c
TTI-DanielChiluisa <daniel@taotechideas.com>
2017-07-05 12:15:48 -0500

    F_AddMarker_01
    Make draggable to marker

--- AdminWeb/public/dashboard/PanelCentroR.js
+++ AdminWeb/public/dashboard/PanelCentroR.js
@@ -56,6 +56,7 @@ $(document).ready(function(){
 						lng: -78.483574,
 						title: 'Centro de Reciclaje',
 						icon: '../iconos/recycle.png',
+                        dragable: true
 					});
 });
 var MapsGoogle = function () {

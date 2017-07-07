82df7e5ea67954b3bd44533b80466c2410ff6730
TTI-DanielChiluisa <daniel@taotechideas.com>
2017-07-07 14:02:01 -0500

    F_ColumnJourney_01
    Delete column Importer

--- AdminWeb/public/dashboard/PanelMonitoreo.js
+++ AdminWeb/public/dashboard/PanelMonitoreo.js
@@ -89,7 +89,8 @@ $(document).ready(function(data){
 			// console.log(driver);
 			$('#ActiveOrders').append("<tbody><tr><td onclick='ShowRouteTest("+j+")'>"+lstJourneys[j].JourneyId+"</td><td onclick='ShowRouteTest("+j+")'>"+
 									lstJourneys[j].JourneyDate+"</td><td onclick='ShowRouteTest("+j+")'>"+lstJourneys[j].truckId+"</td><td onclick='ShowRouteTest("+j+")'>"+RCName+
-									"</td><td onclick='ShowRouteTest("+j+")'>"+importerName+"</td></tr><tbody>");  
+									// "</td><td onclick='ShowRouteTest("+j+")'>"+importerName+"</td></tr><tbody>");  
+									"</td>");  
 		}
 		socket.removeListener("SelectActiveOrders");
     }) 

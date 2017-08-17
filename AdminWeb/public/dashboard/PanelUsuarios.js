00413d1200205ee06d3c7fc89a1108fa868787a2
Unknown <inti@taotechideas.com>
2017-08-17 09:54:46 -0500

    T_ButtonDeleteUser_01
    Change delete by edit

--- AdminWeb/public/dashboard/PanelUsuarios.js
+++ AdminWeb/public/dashboard/PanelUsuarios.js
@@ -33,7 +33,7 @@ function FillTable(){
 			for (var i = 0; i < lstUsers.length; i++) {
 				$('#UsersTable').append("<tbody><tr><td>"+lstUsers[i].person.PERSONCIRUC+"</td><td>"+lstUsers[i].person.PERSONNAME+' '+lstUsers[i].person.PERSONLASTNAME+"</td><td>"+lstUsers[i].user.USEREMAIL+"</td><td>"+lstUsers[i].person.PERSONROLE+
 								   "</td><td><a class='btn red btn-outline sbold' data-toggle='modal' href='#responsive' onclick='ShowUserInformation("+i+")'> <i class='fa fa-edit'> </i> Editar </a>"+
-								   "<a class='btn red btn-outline sbold' data-toggle='modal' href='#responsive' onclick='DeleteUser("+i+")'> <i class='fa fa-delete'> </i> Editar </a></td></tr></tbody>");
+								   "<a class='btn red btn-outline sbold' data-toggle='modal' href='#responsive' onclick='DeleteUser("+i+")'> <i class='fa fa-edit'> </i> Editar </a></td></tr></tbody>");
 			}
 		})
 	})

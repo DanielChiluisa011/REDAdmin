c8b10dd367ff087f82b17b0846de8ebe4832daf1
Unknown <inti@taotechideas.com>
2018-03-02 12:47:14 -0500

    FF_PanelManifiestos_14
    Ivan Rodriguez

--- AdminWeb/public/dashboard/PanelManifiesto.js
+++ AdminWeb/public/dashboard/PanelManifiesto.js
@@ -35,7 +35,7 @@ function FillTable(){
 				
 			date=data[i].JOURNEYDATE.split("T");
 			
-			$('#ManifestTable').append("<tr align='center'><td>"+data[i].ORDERID+"</td><td>"+data[i].JOURNEYID+"</td><td>"+data[i].IMPORTERNAME+"</td><td>"+date[0]+"</td><td>"+data[i].TRUCKID+"</td><td>"+data[i].PERSONNAME+" "+data[i].PERSONLASTNAME+"</td><td><textarea rows='3' cols='30' id='txobservacion"+i+"' onkeydown='ActiveButton("+i+")'>"+obs1+"</textarea></td><td><button id='btnm"+i+"' class='btn green' disabled onclick='UpdateManifest("+i+")'><i class='fa fa-save'></i>Modificar</button></td></tr>");
+			$('#ManifestTable').append("<tr align='center'><td>"+data[i].ORDERID+"</td><td>"+data[i].JOURNEYID+"</td><td>"+data[i].IMPORTERNAME+"</td><td>"+date[0]+"</td><td>"+data[i].TRUCKID+"</td><td>"+data[i].PERSONNAME+" "+data[i].PERSONLASTNAME+"</td><td><textarea rows='3' cols='30' id='txobservacion"+i+"' onkeyup='ActiveButton("+i+")'>"+obs1+"</textarea></td><td><button id='btnm"+i+"' class='btn green' disabled onclick='UpdateManifest("+i+")'><i class='fa fa-save'></i>Modificar</button></td></tr>");
 		}
 		$('#OrdersHistoryTable').append("</tbody>");
 		lstObjManifest=data;
@@ -48,11 +48,11 @@ function ActiveButton(i){
 	var obs=document.getElementById(b2).value;
 	alert(obs.length);
 	if(obs.length==0){
-		document.getElementById(b1).disabled=false;
+		document.getElementById(b1).disabled=true;
 		alert("bloqueo");
 	}
 	else{
-		document.getElementById(b1).disabled=true;
+		document.getElementById(b1).disabled=false;
 		alert("desbloqueo");
 	}
 }

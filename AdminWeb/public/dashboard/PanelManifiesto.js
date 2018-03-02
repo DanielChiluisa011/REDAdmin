d2227c1f90e2a4fe785b15bd9422b80655457279
Unknown <inti@taotechideas.com>
2018-03-02 11:36:14 -0500

    FF_PanelManifiestos_06
    Ivan Rodriguez

--- AdminWeb/public/dashboard/PanelManifiesto.js
+++ AdminWeb/public/dashboard/PanelManifiesto.js
@@ -2,14 +2,7 @@ var socket = io.connect("http://34.195.35.232:8080",{"forceNew": true});
 var lstObjManifest=[];
 $(document).ready(function(){
 	FillTable();
-	// socket.on('NotificationNewUser', function(data){
-	// 	$("#NewUsersTable > tbody").html("");
-	// 	for (var i = 0; i < data.length; i++) {
-	// 			$('#NewUsersTable').append("<tbody><tr><td>"+data[i].person.PersonCi+"</td><td>"+data[i].person.PersonName+' '+data[i].person.PersonLastName+"</td><td>"+data[i].person.PersonAddress+"</td><td>"+data[i].person.PersonPhone+"</td><td>"+data[i].person.PersonRuc+"</td><td>"+data[i].person.PersonRole+"</td><td>"+data[i].user.UserEmail+"</td><td>"+data[i].user.UserProfile+
- //       							"</td><td><button class='btn green' onclick='SaveNewUser("+i+")'>seleccionar</</td></tr></tbody>");
-	// 		}
-	// 	lstObjNewUsers=data;
-	// });
+	
 })
 
 function UpdateManifest(i){
@@ -42,9 +35,14 @@ function FillTable(){
 				
 			date=data[i].JOURNEYDATE.split("T");
 			
-			$('#ManifestTable').append("<tr align='center'><td>"+data[i].ORDERID+"</td><td>"+data[i].JOURNEYID+"</td><td>"+data[i].IMPORTERNAME+"</td><td>"+date[0]+"</td><td>"+data[i].TRUCKID+"</td><td>"+data[i].PERSONNAME+" "+data[i].PERSONLASTNAME+"</td><td><textarea rows='3' cols='30' id='txobservacion"+i+"'>"+obs1+"</textarea></td><td><button class='btn green' onclick='UpdateManifest("+i+")'><i class='fa fa-save'></i>Modificar</button></td></tr>");
+			$('#ManifestTable').append("<tr align='center'><td>"+data[i].ORDERID+"</td><td>"+data[i].JOURNEYID+"</td><td>"+data[i].IMPORTERNAME+"</td><td>"+date[0]+"</td><td>"+data[i].TRUCKID+"</td><td>"+data[i].PERSONNAME+" "+data[i].PERSONLASTNAME+"</td><td><textarea rows='3' cols='30' id='txobservacion"+i+"' onblur='ActiveButton("+i+")'>"+obs1+"</textarea></td><td><button id='btnm"+i+"' class='btn green' disabled onclick='UpdateManifest("+i+")'><i class='fa fa-save'></i>Modificar</button></td></tr>");
 		}
 		$('#OrdersHistoryTable').append("</tbody>");
 		lstObjManifest=data;
 	});
+}
+
+function ActiveButton(i){
+	var b1="btnm"+i;
+	document.getElementById(b1).disabled=false;
 }

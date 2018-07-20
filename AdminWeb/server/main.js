d90b9a9dfcb962d06f616744410a21a89b3f0e31
Unknown <inti@taotechideas.com>
2018-07-20 12:25:43 -0500

    CAMBIO_RED_ERROR_20JULIO2018
    Ivan Rodriguez
    Cambio (Eliana)

--- AdminWeb/server/main.js
+++ AdminWeb/server/main.js
@@ -248,7 +248,7 @@ io.on('connection', function(socket){
       });
       socket.on('RequestJourneyRoute',function(data){
       	console.log('RequestJourneyRoute cedula: '+data);
-      		connection.query("select j.JourneyId, j.JourneyRoute, j.recyclingcenterid, j.truckid from journey j, trucks t, person p where j.truckid=t.TruckId and t.PersonId=p.PersonId and  p.PersonId='"+data+"' AND j.JOURNEYSTATE='Pendiente'",function(error, result){
+      		connection.query("select j.JourneyId, j.JourneyRoute, j.recyclingcenterid, j.truckid from journey j, trucks t, person p where j.truckid=t.TruckId and t.PersonId=p.PersonId and  p.PersonId='"+data+"'",function(error, result){
 				if(error){
 				    throw error;
 				}else{
